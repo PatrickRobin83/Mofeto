ff04ca8e132304a2d06c77874df355fc508a6049
Patrick Robin <patrick-robin@gmx.net>
2020-01-31 00:18:16 +0100

    Written first Tests for Entry Model Class. Added the Car Class

---
+++ Logic/Logic.Ui/Models/Car.cs
@@ -0,0 +1,35 @@
+﻿/*
+*----------------------------------------------------------------------------------
+*          Filename:	Car.cs
+*          Date:        2020.01.30 22:08:20
+*          All rights reserved
+*
+*----------------------------------------------------------------------------------
+* @author Patrick Robin <support@rietrob.de>
+*/
+
+namespace RietRob.Mofeto.Logic.Ui.Models
+
+{
+    public class Car
+    {
+
+        #region Fields
+
+        
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
---
+++ Tests/Tests.LogicUI/EntryTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RietRob.Mofeto.Logic.Ui.Models;
+
+namespace Tests.LogicUI
+{
+    [TestClass]
+    public class EntryTests
+    {
+        private Record _testTarget;
+        private DateTime _date;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _date = new DateTime(2020,01,03);
+            _testTarget = new Record(_date, 1.389, 448.10, 23.57, new Car());
+        }
+
+        [TestMethod]
+        public void CalculateTheTotalAmountInLiter()
+        {
+            Assert.AreEqual(32.74,Math.Round(_testTarget.TotalPrice,2));
+        }
+
+        [TestMethod]
+        public void CalculateTheConsumptionOfHundredKilometer()
+        {
+            Assert.AreEqual(5.26,Math.Round(_testTarget.GetConsumptionForHundredKilometer(),2));
+        }
+
+        [TestMethod]
+        public void CalculateTheCostsForHundredKilometer()
+        {
+            Assert.AreEqual(7.31,Math.Round(_testTarget.CostsForHundredKilometer,2));
+        }
+    }
+}
